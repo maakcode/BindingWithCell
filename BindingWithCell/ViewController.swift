3086a9c28ad7ac75e66a3cd135b7025725c5714e
Makeeyaf <makeeyaf@gmail.com>
2021-01-15 18:59:35 +0900

    Initial Commit

---
+++ BindingWithCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BindingWithCell
+//
+//  Created by Makeeyaf on 2021/01/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
